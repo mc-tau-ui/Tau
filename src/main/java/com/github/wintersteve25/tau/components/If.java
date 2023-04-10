4d61b5b5df10631c13342dcc6ff483aaff0bce6b
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-04-10 12:33:18 -0400

    If is now a primitive ui component

--- src/main/java/com/github/wintersteve25/tau/components/If.java
+++ src/main/java/com/github/wintersteve25/tau/components/If.java
@@ -1,27 +1,29 @@
 package com.github.wintersteve25.tau.components;
 
+import com.github.wintersteve25.tau.build.UIBuilder;
+import com.github.wintersteve25.tau.components.base.DynamicUIComponent;
+import com.github.wintersteve25.tau.components.base.PrimitiveUIComponent;
 import com.github.wintersteve25.tau.components.base.UIComponent;
 import com.github.wintersteve25.tau.layout.Layout;
+import com.github.wintersteve25.tau.utils.Vector2i;
+import net.minecraft.client.gui.IGuiEventListener;
+import net.minecraft.client.gui.IRenderable;
 
-import java.util.function.Supplier;
+import java.util.List;
 
-public class If implements UIComponent {
+public class If implements PrimitiveUIComponent {
     
-    private final Supplier<Boolean> condition;
+    private final boolean condition;
     private final UIComponent child;
 
-    public If(Supplier<Boolean> condition, UIComponent child) {
+    public If(boolean condition, UIComponent child) {
         this.condition = condition;
         this.child = child;
     }
 
     @Override
-    public UIComponent build(Layout layout) {
-        if (condition.get()) {
-            return child;
-        }
-        
-        return new Container.Builder()
-                .noColor();
+    public Vector2i build(Layout layout, List<IRenderable> renderables, List<IRenderable> tooltips, List<DynamicUIComponent> dynamicUIComponents, List<IGuiEventListener> eventListeners) {
+        if (condition) return UIBuilder.build(layout, child, renderables, tooltips, dynamicUIComponents, eventListeners);
+        return layout.getSize();
     }
 }
